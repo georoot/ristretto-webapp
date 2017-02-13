5ba6056531fa1bc7415ee80ad23583e885a4459a
georoot <rb1223334444@gmail.com>
2017-02-13 20:50:49 +0530

    Basic settings page

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -5,10 +5,24 @@ module.exports = ($scope,$localStorage,Restangular)=>{
       .one("user/me")
       .get(undefined,{Authorization: $scope.token})
       .then((data)=>{
-        $scope.user = data;
+        $scope.users = data;
+        console.log(data);
       })
       .catch((err)=>{
         console.log(err);
+      });
+  }
+
+  $scope.addKey = ()=>{
+    var dataAtom = {title:$scope.form.title,value:$scope.form.value};
+    $scope.users.keys.push(dataAtom);
+    $scope.users
+      .put(undefined,{Authorization: $scope.token})
+      .then((data)=>{
+        $scope.getUser();
+      })
+      .catch((err)=>{
+        alert("Error while adding keys");
       });
   }
 
